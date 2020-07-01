8ff9f3e848dad8efa06b349fcac4c19fafa6ca18
Abhishek Kumar Rudra <abhishek.rudra02@gmail.com>
2020-07-01 14:56:05 +0530

    first cut of ufile

--- src/main/java/com/strandls/resource/ApiConstants.java
+++ src/main/java/com/strandls/resource/ApiConstants.java
@@ -20,5 +20,8 @@ public class ApiConstants {
 	public static final String UPDATE = "/update";
 	public static final String LICENSE = "/lisence";
 	public static final String RATING = "/rating";
+	public static final String UFILE = "/ufile";
+	
+	
 
 }
